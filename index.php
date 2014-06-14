482271c73b8c0f8c3ca2e500aa74d5fe531c97ad
晓白 <suyang.sy@alibaba-inc.com>
2014-06-15 02:57:29 +0800

    upload files.
    Signed-off-by: 晓白 <suyang.sy@alibaba-inc.com>

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name: Replace Google Libs
+ * Plugin URI:  http://www.soulteary.com/2014/06/15/replace-google-libs.html
+ * Description: Use Qihoo 360 Open Libs Service to replace Google's.
+ * Author:      soulteary
+ * Author URI:  http://www.soulteary.com/
+ * Version:     1.0
+ * License:     GPL
+ */
+
+
+/**
+ * Silence is golden
+ */
+if (!defined('ABSPATH')) exit;
+
+class Replace_Google_Libs
+{
+
+    /**
+     * init Hook
+     *
+     */
+    public function __construct()
+    {
+        add_filter('wp_print_scripts', array($this, 'ohMyScript'), 1000, 1);
+    }
+
+
+    /**
+     * Use Qihoo 360 Open Libs Service to replace Google's.
+     *
+     * @param $text
+     * @return mixed
+     */
+    public function ohMyScript()
+    {
+        global $wp_scripts;
+        if($wp_scripts && $wp_scripts->registered){
+            foreach ($wp_scripts->registered as $libs){
+                $libs->src = str_replace('//ajax.googleapis.com', '//ajax.useso.com/', $libs->src);
+            }
+        }
+    }
+}
+
+/**
+ * bootstrap
+ */
+new Replace_Google_Libs;
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Replace Google Libs ===
+
+Contributors: soulteary
+Tags: Qihu Libs, Qihu Open Libs, 360 Libs, 360 Open Libs, Google Libs, Google Open Libs
+Requires at least: 3.5
+Tested up to: 3.8
+Stable tag: 1.0
+
+Use Qihoo 360 Open Libs Service to replace Google's.
+
+== Description ==
+
+[Plugin homepage](http://www.soulteary.com/2014/06/15/replace-google-libs.html) | [Plugin author](http://www.soulteary.com/)
+
+There are some problem with Google in china, the Google Libs make the website too slow to open, so we can use Qihoo 360's Libs replace the Google's.
+
+[Project GitHub](https://github.com/soulteary/Replace-Google-Libs).
+
+== Installation ==
+
+1. Upload `replace-google-libs` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
